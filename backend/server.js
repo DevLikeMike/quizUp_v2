b254a1917827ff33d82f3900eeca0800ec643e81
DevLikeMike <michael@MacBook-Pro.local>
2021-01-25 14:40:20 -0500

    User auth set up, and front end ready for start

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 import express from "express";
 import colors from "colors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+// Route imports
+import userRoutes from "./routes/userRoutes.js";
+// Middleware
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
+dotenv.config();
+connectDB();
 const app = express();
+app.use(express.json());
 
+// Routes
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+// Port declaration
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => {
